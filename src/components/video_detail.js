dd41ca085ee21d3bfe5b5968f8223750d147dcae
Albin Bäckström <albinbackstrom@Hampuss-iMac.local>
2018-09-13 16:36:34 +0200

    commit

---
+++ src/components/video_detail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+
+    //Kontrollerar om det har laddats någon video. Returnerar ev div med text för att sedan l
+    //visa videorna
+    if (!video) {
+        return <div>Loading...</div>
+    }
+
+    const videoID = video.id.videoId;
+    const url = `https://www.youtube.com/embed/${videoID}`;
+    console.log(url);
+
+
+    return (
+        <div className="video-detail col-md-8">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src={url} frameborder="0"></iframe>
+            </div>
+            <div className="video-detail">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
 const API_KEY = "AIzaSyBosbrSs7FAfxk8MH5RzUIVzdT4w-P7AsA";
 
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    )
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+        console.log(this.state);
+
+
+        YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+            this.setState({videos})
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+
+        )
+    }
+}
 
 const root = document.getElementById('root');
 ReactDOM.render(<App/>, root);
